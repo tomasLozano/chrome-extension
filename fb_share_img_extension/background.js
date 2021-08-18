0491447687a1309844db6bb1d8a8b4e6ca47ba77
TomasLozano <tomaslozanol@gmail.com>
2021-08-18 10:49:05 -0400

    yo

---
+++ fb_share_img_extension/background.js
@@ -0,0 +1,37 @@
+
+function MyGenericClick(info, tab) {
+    console.log("Clicked on page: ", info, tab)
+};
+
+function MyImageClick (info, tab) {
+    console.log("Clicked in image: ", info, tab)
+    chrome.windows.create({
+        "url": "https://facebook.com/sharer.php?u=" + info.srcUrl + "&display=popup",
+        "type": "popup"
+    })
+};
+
+function MyQuoteClick (info, tab) {
+    console.log("Clicked in image: ", info, tab)
+    chrome.windows.create({
+        "url": "https://facebook.com/sharer.php?u=" + info.pageUrl + "&display=popup&quote=" + info.selectionText,
+        "type": "popup"
+    })
+};
+
+chrome.contextMenus.create({
+    "title": "Share Image",
+    "contexts": ["image"],
+    "onclick": MyImageClick
+});
+
+chrome.contextMenus.create({
+    "title": "Share Quote",
+    "contexts": ["selection"],
+    "onclick": MyQuoteClick
+});
+
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
+    console.log("message", msg)
+    sendResponse({"text": "Received the links"})
+});
